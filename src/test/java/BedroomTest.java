38d0f2ad49b1958bc18cdf1d310a0120c546a935
Daniel Childs <dc.1994@outlook.com>
2018-05-05 21:23:21 +0100

    create guestCheckOutFromBedroom method and pass test

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -15,48 +15,50 @@ public class BedroomTest {
     Hotel hotel;
 
     @Before
-    public void before(){
+    public void before() {
         bedroom = new Bedroom(1, BedroomType.SINGLE);
         guest = new Guest();
         hotel = new Hotel();
-        bedroom.checkGuestIntoBedroom(hotel);
     }
 
     @Test
-    public void GetRoomNumber(){
+    public void GetRoomNumber() {
         assertEquals(1, bedroom.getRoomNumber());
     }
 
     @Test
-    public void GetRoomType(){
+    public void GetRoomType() {
         assertEquals(SINGLE, bedroom.getType());
     }
 
     @Test
-    public void GetPrice(){
+    public void GetPrice() {
         assertEquals(50.00, bedroom.getPrice(), 0.01);
     }
 
     @Test
-    public void GetCapacity(){
+    public void GetCapacity() {
         assertEquals(1, bedroom.getCapacity());
     }
+
     @Test
-    public void canGetGuest(){
+    public void canGetGuest() {
         assertEquals(0, bedroom.guestCount());
     }
 
     @Test
-    public void canCheckGuestIntoBedroom(){
+    public void canCheckGuestIntoBedroom() {
         hotel.checkIn(guest);
         bedroom.checkGuestIntoBedroom(hotel);
         assertEquals(1, bedroom.guestCount());
     }
 
-//    @Test
-//    public void canCheckGuestOutFromBedroom(){
-//
-//        bedroom.checkGuestOutFromBedroom();
-//        assertEquals(0, bedroom.guestCount());
-//    }
+    @Test
+    public void canCheckGuestOutFromBedroom(){
+        hotel.checkIn(guest);
+        bedroom.checkGuestIntoBedroom(hotel);
+        bedroom.checkGuestOutFromBedroom();
+        assertEquals(0, bedroom.guestCount());
+    }
 }
+
